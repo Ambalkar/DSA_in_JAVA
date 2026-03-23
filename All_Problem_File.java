ffe7ccb4e6ac066e3686836cc15a89e96bcead1f
Ambalkar <devendraambalkar11@gmail.com>
2026-03-23 11:59:14 +0530

    Day 2 Inverted star pattern

--- All_Problem_File.java
+++ All_Problem_File.java
@@ -1,23 +1,70 @@
 package DSA_in_JAVA;
 
 public class All_Problem_File {
+
 	
-	/*	Problem 1: print star pattern
-	 * 	*	
-	 * 	**
-	 * 	***
-	 * 	****
-	 * 
-	 * */ 
-	//Solution 1:
+//	 //Problem 1: print star pattern 
+//	
+//	*
+//	**
+//	***
+//	****
+//	 
+//	 
+//		// Solution 1:
+//		public static void main(String[] args) {
+//			for (int line = 0; line <= 4; line++) {
+//				for (int star = 0; star <= line; star++) {
+//					System.out.print("*");
+//				}
+//				System.out.println();
+//			}
+//		}
+//
+//	}
+// 
+//
+// 	//Problem 2: inverted star pattern
+// 
+// 
+// ****
+// ***
+// **
+// *
+// 
+//	  //Solution 2:
+//	  
 	public static void main(String[] args) {
-		for(int line = 0;line<=4;line++) {
-			for(int star=0;star>=line;star++) {
+		int n=4;
+		for(int line=1;line<=n;line++) {
+			for(int star =1;star<=n-line+1;star++) {
 				System.out.print("*");
 			}
-			System.out.println();
+		System.out.println();
+		
 		}
+		
 	}
+}	
 	
-
-}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
