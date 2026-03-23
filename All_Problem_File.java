408e4069b789802077febcd6f0fa727b95f19268
Ambalkar <devendraambalkar11@gmail.com>
2026-03-23 12:20:04 +0530

    Day 2.1 Half pyramid pattern

--- All_Problem_File.java
+++ All_Problem_File.java
@@ -34,20 +34,40 @@ public class All_Problem_File {
 // 
 //	  //Solution 2:
 //	  
-	public static void main(String[] args) {
-		int n=4;
-		for(int line=1;line<=n;line++) {
-			for(int star =1;star<=n-line+1;star++) {
-				System.out.print("*");
-			}
-		System.out.println();
-		
-		}
-		
-	}
-}	
-	
-	
+//	public static void main(String[] args) {
+//		int n=4;
+//		for(int line=1;line<=n;line++) {
+//			for(int star =1;star<=n-line+1;star++) {
+//				System.out.print("*");
+//			}
+//		System.out.println();
+//		
+//		}
+//		
+//	}
+//}	
+//
+//		Problem 3: Print Half pyramid pattern
+//	1
+//	12
+//	123
+//	1234
+//	
+//	
+//		Solution 3:
+//
+//	public static void main(String[] args) {
+//		for(int line = 1; line<=4; line++) {
+//			for(int number = 1; number<=line; number++) {
+//				System.out.print(number);
+//			}
+//			System.out.println();
+//		}
+//			
+//	}
+//
+//}
+
 	
 	
 	
