0e7d5d6d9bd633a0aa85ef6aeb50093115b88905
Ambalkar <devendraambalkar11@gmail.com>
2026-03-24 18:47:45 +0530

    Day 3 Print Character Pattern

--- All_Problem_File.java
+++ All_Problem_File.java
@@ -67,7 +67,28 @@ public class All_Problem_File {
 //	}
 //
 //}
-
+//		Problem 4: Print Character Pattern
+//	 A
+//	 BC
+//	 DEF
+//	 GHIJ
+	 
+	
+//		Solution 4: 
+	
+	public static void main(String[] args) {
+		int n = 4;
+		char ch = 'A';
+		for(int line = 1; line<=n;line++) {
+			for(int chars =1; chars<=line; chars++) {
+				System.out.print(ch);
+				ch++;
+			}
+			System.out.println();
+		}
+	}
+	
+}
 	
 	
 	
