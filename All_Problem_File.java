03c2137fa9bb4e013122f5617030ca191dacbd45
Ambalkar <devendraambalkar11@gmail.com>
2026-04-08 17:43:42 +0530

    Day 12 Palindrome number(Leet)

--- All_Problem_File.java
+++ All_Problem_File.java
@@ -305,4 +305,30 @@ public class All_Problem_File {
 //
 //Output:
 //	The key is at the index : 4
+// 
+//	
+//	
+//	Problem 10: Check if the number is  Palindrome number
+//	
+//	Solution 10: 
+	
+	public static void main(String[] args) {
+		
+		int num = 121;
+		int original = num;
+		int reversed = 0;
+		while(num !=0) {
+			int digit = num%10;
+			reversed = reversed * 10 + digit;
+			num = num/10;
+			
+			
+		}if(original==reversed) {
+			System.out.println("The number is palindrome");
+		}else {
+			System.out.println("The number is not palindrome");
+		}
+	}
+}
+
 
