fd549f4cef74c0ad7d16b0e425f68e6acdf1f7f9
Devendra Ambalkar <93560430+Ambalkar@users.noreply.github.com>
2026-07-10 17:13:51 +0530

    Time: 1 ms (100%), Space: 43.9 MB (15.58%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -0,0 +1,43 @@
+class Solution {
+
+    private Boolean[][] dp;
+
+    public boolean isMatch(String s, String p) {
+
+        dp = new Boolean[s.length() + 1][p.length() + 1];
+
+        return solve(0, 0, s, p);
+    }
+
+    private boolean solve(int i, int j, String s, String p) {
+
+        // Already calculated
+        if (dp[i][j] != null)
+            return dp[i][j];
+
+        // Pattern finished
+        if (j == p.length())
+            return dp[i][j] = (i == s.length());
+
+        // Current characters match?
+        boolean firstMatch = (i < s.length()) &&
+                (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
+
+        // Next character is '*'
+        if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+
+            dp[i][j] =
+                    solve(i, j + 2, s, p) ||
+                    (firstMatch && solve(i + 1, j, s, p));
+
+        } else {
+
+            dp[i][j] =
+                    firstMatch &&
+                    solve(i + 1, j + 1, s, p);
+
+        }
+
+        return dp[i][j];
+    }
+}
